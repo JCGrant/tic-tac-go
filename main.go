f61902cb8ef91d8ac2865c06bae6ab2e80ca9969
JCGrant <jamescolin.grant@gmail.com>
2018-08-05 18:55:33 +0100

    Implement user input and setting tiles

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+)
 
 type Tile rune
 type Board [][]Tile
@@ -35,7 +41,40 @@ func (b *Board) String() (result string) {
 	return
 }
 
+func (b *Board) MustSetTile(tile Tile, x int, y int) {
+	[][]Tile(*b)[y][x] = tile
+}
+
+var inputRegExp = regexp.MustCompile(`(\d+)\s*,?\s*(\d+)`)
+
+func getInput(prompt string) string {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Printf("%s: ", prompt)
+	text, _ := reader.ReadString('\n')
+	return text
+}
+
+func getXY() ([]int, error) {
+	input := getInput("x, y")
+	output := inputRegExp.FindStringSubmatch(input)
+	if output == nil {
+		return nil, fmt.Errorf("invalid x or y")
+	}
+	xy := make([]int, 2, 2)
+	for i, digitStr := range output[1:] {
+		digit, _ := strconv.Atoi(digitStr)
+		xy[i] = digit
+	}
+	return xy, nil
+}
+
 func main() {
 	b := NewBoard()
+	fmt.Println(b)
+	xy, err := getXY()
+	if err != nil {
+		fmt.Printf("getting x and y failed: %s", err)
+	}
+	b.MustSetTile(cross, xy[0], xy[1])
 	fmt.Println(b)
 }
